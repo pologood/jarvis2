a112be4bb0dd92ddda954e2a3ae808a6ad0c62d5
guangming <guangming@guangmingdeMacBook-Pro-2.local>
2015-09-01 10:32:06 +0800

    DAGScheduler 增删改查

--- jarvis-server/src/main/java/com/mogujie/jarvis/server/scheduler/dag/DAGScheduler.java
+++ jarvis-server/src/main/java/com/mogujie/jarvis/server/scheduler/dag/DAGScheduler.java
@@ -8,6 +8,8 @@
 
 package com.mogujie.jarvis.server.scheduler.dag;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -50,6 +52,11 @@ public class DAGScheduler extends AbstractScheduler {
 
     }
 
+    /**
+     * add job
+     *
+     * @param jobDesc
+     */
     public void addJob(JobDescriptor jobDesc) throws Exception {
         int jobid = (int) jobDesc.getJobContext().getJobId();
         if (waitingTable.get(jobid) == null) {
@@ -71,16 +78,102 @@ public class DAGScheduler extends AbstractScheduler {
         }
     }
 
+    /**
+     * remove job
+     *
+     * @param jobDesc
+     */
     public void removeJob(JobDescriptor jobDesc) {
+        int jobid = (int) jobDesc.getJobContext().getJobId();
+        DAGJob dagJob = waitingTable.get(jobid);
+        if (dagJob != null) {
+            // 1. remove job from waiting table
+            waitingTable.remove(dagJob);
 
+            // 2. remove relation from parents
+            List<DAGJob> parents = dagJob.getParents();
+            for (DAGJob p : parents) {
+                p.removeChild(dagJob);
+                dagJob.removeParent(p);
+            }
+
+            // 3. remove relation from children
+            List<DAGJob> children = dagJob.getChildren();
+            for (DAGJob c : children) {
+               dagJob.removeChild(c);
+               c.removeParent(dagJob);
+            }
+        }
     }
 
-    public void addDependency(JobDescriptor parent, JobDescriptor child) {
-
+    /**
+     * add dependency
+     *
+     * @param jobDesc parent
+     * @param jobDesc child
+     */
+    public void addDependency(JobDescriptor jp, JobDescriptor jc) {
+        DAGJob parent = waitingTable.get(jp.getJobContext().getJobId());
+        DAGJob child = waitingTable.get(jc.getJobContext().getJobId());
+        if (parent != null && child != null) {
+            parent.addChild(child);
+            child.addParent(parent);
+        }
     }
 
-    public void removeDependency(JobDescriptor parent, JobDescriptor child) {
+    /**
+     * remove dependency
+     *
+     * @param jobDesc parent
+     * @param jobDesc child
+     */
+    public void removeDependency(JobDescriptor jp, JobDescriptor jc) {
+        DAGJob parent = waitingTable.get(jp.getJobContext().getJobId());
+        DAGJob child = waitingTable.get(jc.getJobContext().getJobId());
+        if (parent != null && child != null) {
+            parent.removeChild(child);
+            child.removeParent(parent);
+        }
+    }
 
+    /**
+     * get dependency parent
+     *
+     * @param jobDesc
+     */
+    public List<Integer> getParents(JobDescriptor jobDesc) {
+        List<Integer> parentIds = new ArrayList<Integer>();
+        DAGJob dagJob = waitingTable.get(jobDesc.getJobContext().getJobId());
+        if (dagJob != null) {
+            List<DAGJob> parentJobs = dagJob.getParents();
+            if (parentJobs != null) {
+                for (DAGJob p : parentJobs) {
+                    parentIds.add(p.getJobid());
+                }
+            }
+        }
+
+        return parentIds;
+    }
+
+    /**
+     * get subsequent child
+     *
+     * @param jobDesc
+     */
+    public List<Integer> getChildren(JobDescriptor jobDesc) {
+        List<Integer> childIds = new ArrayList<Integer>();
+        DAGJob dagJob = waitingTable.get(jobDesc.getJobContext().getJobId());
+        if (dagJob != null) {
+            List<DAGJob> childJobs = dagJob.getParents();
+            if (childJobs != null) {
+                for (DAGJob c : childJobs) {
+                    childIds.add(c.getJobid());
+                }
+            }
+        }
+
+        return childIds;
     }
 
 }
