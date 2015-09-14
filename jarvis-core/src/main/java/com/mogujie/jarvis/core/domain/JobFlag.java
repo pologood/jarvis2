465b170dd70a906d437a3b235f9f4081b206fe20
muming <muming@mogujie.com>
2015-09-14 11:20:53 +0800

    jobFlag

---
+++ jarvis-core/src/main/java/com/mogujie/jarvis/core/domain/JobFlag.java
@@ -0,0 +1,42 @@
+/*
+ * 蘑菇街 Inc.
+ * Copyright (c) 2010-2015 All Rights Reserved.
+ *
+ * Author: wuya
+ * Create Date: 2015年6月12日 下午4:56:39
+ */
+package com.mogujie.jarvis.core.domain;
+
+/**
+ * @author muming
+ *
+ */
+public enum JobFlag {
+
+    ENABLE(1), //启用（有效）
+    DISABLE(2), //禁用（失效）
+    DELETED(3);   //垃圾箱
+
+    private int value;
+
+    JobFlag(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static JobFlag getInstance(int value) {
+        JobFlag[] statusList = JobFlag.values();
+        JobFlag status = JobFlag.ENABLE;
+        for (JobFlag s : statusList) {
+            if (s.getValue() == value) {
+                status = s;
+                break;
+            }
+        }
+
+        return status;
+    }
+}
