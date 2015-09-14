f3deff51118fa818882032ecdb96695b989b9978
muming <muming@mogujie.com>
2015-09-14 18:04:04 +0800

    diy

--- jarvis-dao/src/main/java/com/mogujie/jarvis/dao/diy/MyMapper.java
+++ jarvis-dao/src/main/java/com/mogujie/jarvis/dao/diy/MyMapper.java
@@ -2,8 +2,9 @@ package com.mogujie.jarvis.dao.diy;
 
 import com.mogujie.jarvis.dto.Crontab;
 import com.mogujie.jarvis.dto.CrontabExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MyMapper {
     /**
