85fb4581302f0e9fe793560eb2153aaeafb3fb5e
hejian <hejian@mogujie.com>
2015-09-23 20:02:15 +0800

    no message

--- jarvis-web/src/main/java/com/mogujie/jarvis/web/mapper/jobDependMapper.java
+++ jarvis-web/src/main/java/com/mogujie/jarvis/web/mapper/jobDependMapper.java
@@ -8,6 +8,6 @@ import java.util.List;
  * Created by hejian on 15/9/22.
  */
 public interface JobDependMapper {
-    public JobDependVo getJobById(Long jobId);
+    public JobDependVo getJobById(Long jobId); 
     public List<JobDependVo> getChildrenById(Long jobId);
 }
