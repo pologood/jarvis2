577a1616e34fe06841c9e5b25be628538f153f78
hejian <hejian@mogujie.com>
2015-09-29 14:04:12 +0800

    no message

--- jarvis-web/src/main/java/com/mogujie/jarvis/web/controller/api/JobAPIController.java
+++ jarvis-web/src/main/java/com/mogujie/jarvis/web/controller/api/JobAPIController.java
@@ -6,12 +6,8 @@ import com.mogujie.jarvis.web.service.JobDependService;
 import com.mogujie.jarvis.web.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by hejian on 15/9/15.
