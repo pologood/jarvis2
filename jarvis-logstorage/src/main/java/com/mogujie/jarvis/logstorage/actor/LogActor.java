e7b3170be9572a5042b54cd505fdbffc2c59288a
muming <muming@mogujie.com>
2015-10-14 20:52:10 +0800

    logActor edit

--- jarvis-logstorage/src/main/java/com/mogujie/jarvis/logstorage/actor/LogActor.java
+++ jarvis-logstorage/src/main/java/com/mogujie/jarvis/logstorage/actor/LogActor.java
@@ -13,6 +13,8 @@ import com.mogujie.jarvis.logstorage.domain.LogReadResult;
 import com.mogujie.jarvis.logstorage.util.LogUtil;
 import com.mogujie.jarvis.protocol.ReadLogProtos.LogServerReadLogResponse;
 import com.mogujie.jarvis.protocol.ReadLogProtos.RestServerReadLogRequest;
+import com.mogujie.jarvis.protocol.WriteLogProtos.LogServerWriteLogResponse;
+import com.mogujie.jarvis.protocol.WriteLogProtos.WorkerWriteLogRequest;
 
 import akka.actor.Props;
 import akka.actor.UntypedActor;
@@ -20,20 +22,22 @@ import akka.actor.UntypedActor;
 /**
  * @author 牧名
  */
-public class LogReadActor extends UntypedActor {
+public class LogActor extends UntypedActor {
 
     public static Props props() {
-        return Props.create(LogReadActor.class);
+        return Props.create(LogActor.class);
     }
 
     @Override
     public void onReceive(Object obj) throws Exception {
 
         if (obj instanceof RestServerReadLogRequest) {
+            readLog((RestServerReadLogRequest) obj);
 
-            dealRestServerReadLogRequest((RestServerReadLogRequest) obj);
+        } else if (obj instanceof WorkerWriteLogRequest) {
+            writeLog((WorkerWriteLogRequest) obj);
 
-        }else{
+        } else {
             unhandled(obj);
         }
 
@@ -45,7 +49,7 @@ public class LogReadActor extends UntypedActor {
      * @param msg
      * @throws Exception
      */
-    private void dealRestServerReadLogRequest(RestServerReadLogRequest msg) throws Exception{
+    private void readLog(RestServerReadLogRequest msg) throws Exception {
 
         //fullID
         String fullId = msg.getFullId();
@@ -70,6 +74,41 @@ public class LogReadActor extends UntypedActor {
 
     }
 
+
+    /**
+     * 消息处理——写日志
+     *
+     * @param msg
+     * @throws Exception
+     */
+    private void writeLog(WorkerWriteLogRequest msg) throws Exception {
+
+        String fullId = msg.getFullId();
+        StreamType streamType = StreamType.getInstance(msg.getType());
+        String log = msg.getLog();
+        Boolean isEnd = msg.getIsEnd();
+
+        //获取文件路径
+        String filePath = LogUtil.getLogPath4Local(fullId, streamType);
+
+        //写log到本地文件
+        LogUtil.writeLine4Local(filePath, log);
+
+        //log是否结束
+        if (isEnd) {
+            LogUtil.writeEndFlag2Local(filePath);
+        }
+
+        //响应值_做成
+        LogServerWriteLogResponse response;
+        response = LogServerWriteLogResponse.newBuilder()
+                .setSuccess(true)
+                .build();
+
+        //响应值_返回
+        getSender().tell(response, getSelf());
+
+    }
 
 
 }
