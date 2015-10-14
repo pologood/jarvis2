7076ff099ae30c102d583a48260aed21b76d1802
guangming <guangming@guangmingdeMacBook-Pro-2.local>
2015-10-14 19:55:53 +0800

    fix OOM

--- jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestJobActor.java
+++ jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestJobActor.java
@@ -7,23 +7,15 @@
  */
 package com.mogujie.jarvis.server.actor;
 
-import java.util.concurrent.TimeUnit;
-
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 
 import scala.concurrent.Await;
 import scala.concurrent.Future;
-import scala.concurrent.duration.Duration;
 import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.pattern.Patterns;
 import akka.testkit.JavaTestKit;
-import akka.util.Timeout;
 
 import com.mogujie.jarvis.protocol.DependencyEntryProtos.DependencyEntry;
 import com.mogujie.jarvis.protocol.DependencyEntryProtos.DependencyEntry.DependencyOperator;
@@ -34,33 +26,15 @@ import com.mogujie.jarvis.protocol.QueryJobRelationProtos.RestServerQueryJobRela
 import com.mogujie.jarvis.protocol.QueryJobRelationProtos.ServerQueryJobRelationResponse;
 import com.mogujie.jarvis.protocol.SubmitJobProtos.RestServerSubmitJobRequest;
 import com.mogujie.jarvis.protocol.SubmitJobProtos.ServerSubmitJobResponse;
-import com.mogujie.jarvis.server.JarvisServerActorSystem;
 import com.mogujie.jarvis.server.domain.RemoveJobRequest;
 import com.mogujie.jarvis.server.scheduler.dag.strategy.CommonStrategy;
-import com.mogujie.jarvis.server.util.SpringContext;
 import com.mogujie.jarvis.server.util.SpringExtension;
 
 /**
  * @author guangming
  *
  */
-public class TestJobActor {
-    private static ActorSystem system;
-
-    private static final Timeout TIMEOUT = new Timeout(Duration.create(5, TimeUnit.SECONDS));
-
-    @BeforeClass
-    public static void setup() {
-        ApplicationContext context = SpringContext.getApplicationContext();
-        system = JarvisServerActorSystem.getInstance();
-        SpringExtension.SPRING_EXT_PROVIDER.get(system).initialize(context);
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        JavaTestKit.shutdownActorSystem(system);
-    }
-
+public class TestJobActor extends TestBaseActor {
     @Test
     public void testSubmitJob1() {
         new JavaTestKit(system) {{
