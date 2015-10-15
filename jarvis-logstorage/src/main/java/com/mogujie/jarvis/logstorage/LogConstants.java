d441762d60494029948540f07ec16722a724ab56
muming <muming@mogujie.com>
2015-10-15 12:07:55 +0800

    logStorage

--- jarvis-logstorage/src/main/java/com/mogujie/jarvis/logstorage/LogConstants.java
+++ jarvis-logstorage/src/main/java/com/mogujie/jarvis/logstorage/LogConstants.java
@@ -3,7 +3,7 @@ package com.mogujie.jarvis.logstorage;
 /**
  * Created by muming on 15/10/15.
  */
-public class LogConstants {
+public interface LogConstants {
 
     public static final int READ_MAX_LINES = 10000;    //每次读取最大行数
 
