97c4ac5a8cec1712b83fc22565d7676a669f1d9a
wuya <wuya@mogujie.com>
2015-10-19 15:28:22 +0800

    update mybatis ehcache configuration

--- jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestAppActor.java
+++ jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestAppActor.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mogujie.jarvis.protocol.AppAuthProtos.AppAuth;
+import com.mogujie.jarvis.protocol.ApplicationProtos.RestServerCreateApplicationRequest;
+import com.mogujie.jarvis.server.util.AppTokenUtils;
+import com.mogujie.jarvis.server.util.SpringExtension;
+
 import akka.actor.ActorRef;
 import akka.testkit.JavaTestKit;
-
-import com.mogujie.jarvis.protocol.ApplicationProtos.RestServerCreateApplicationRequest;
-import com.mogujie.jarvis.server.util.SpringExtension;
 
 public class TestAppActor extends TestBaseActor {
 
@@ -18,8 +20,10 @@ public class TestAppActor extends TestBaseActor {
     public void test1() {
         new JavaTestKit(system) {
             {
+                AppAuth appAuth = AppAuth.newBuilder().setName("jarvis-web")
+                        .setToken(AppTokenUtils.generateToken(System.currentTimeMillis() / 1000, "11111")).build();
                 final ActorRef ref = system.actorOf(SpringExtension.SPRING_EXT_PROVIDER.get(system).props("appActor"));
-                RestServerCreateApplicationRequest request = RestServerCreateApplicationRequest.newBuilder().setAppName("test-2")
+                RestServerCreateApplicationRequest request = RestServerCreateApplicationRequest.newBuilder().setAppAuth(appAuth).setAppName("test-2")
                         .setMaxConcurrency(789).setStatus(0).setUser("wuya").build();
                 ref.tell(request, getRef());
             }
