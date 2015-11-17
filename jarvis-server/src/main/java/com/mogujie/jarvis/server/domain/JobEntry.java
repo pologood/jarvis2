a5520f68d80a27eb3c3af1d2b056881b7f3581e4
muming <muming@mogujie.com>
2015-11-17 09:56:23 +0800

    debug

---
+++ jarvis-server/src/main/java/com/mogujie/jarvis/server/domain/JobEntry.java
@@ -0,0 +1,37 @@
+package com.mogujie.jarvis.server.domain;
+
+import com.mogujie.jarvis.core.expression.ScheduleExpression;
+import com.mogujie.jarvis.dto.Job;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by muming on 15/11/16.
+ */
+public class JobEntry {
+
+    private final Job job;
+    private final List<ScheduleExpression> scheduleExpressions;
+    private final Map<Long, JobDependencyEntry> dependencies;
+
+    public JobEntry(Job job, List<ScheduleExpression> scheduleExpressions, Map<Long, JobDependencyEntry> dependencies) {
+        this.job = job;
+        this.scheduleExpressions = scheduleExpressions;
+        this.dependencies = dependencies;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public List<ScheduleExpression> getScheduleExpressions() {
+        return scheduleExpressions;
+    }
+
+    public Map<Long, JobDependencyEntry> getDependencies() {
+        return dependencies;
+    }
+
+
+}
--- jarvis-server/src/main/java/com/mogujie/jarvis/server/service/JobService.java
+++ jarvis-server/src/main/java/com/mogujie/jarvis/server/service/JobService.java
@@ -7,10 +7,17 @@
  */
 package com.mogujie.jarvis.server.service;
 
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.mogujie.jarvis.core.expression.*;
+import com.mogujie.jarvis.dao.JobDependMapper;
+import com.mogujie.jarvis.dao.JobScheduleExpressionMapper;
+import com.mogujie.jarvis.dto.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,8 +26,9 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.mogujie.jarvis.core.domain.JobFlag;
 import com.mogujie.jarvis.dao.JobMapper;
-import com.mogujie.jarvis.dto.Job;
-import com.mogujie.jarvis.dto.JobExample;
+
+import com.mogujie.jarvis.server.domain.JobDependencyEntry;
+import com.mogujie.jarvis.server.domain.JobEntry;
 
 /**
  * @author wuya
@@ -29,8 +37,40 @@ import com.mogujie.jarvis.dto.JobExample;
 @Service
 public class JobService {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Autowired
     private JobMapper jobMapper;
+    @Autowired
+    private JobScheduleExpressionMapper jobScheduleExpressionMapper;
+    @Autowired
+    private JobDependMapper jobDependMapper;
+
+    private Map<Long, JobEntry> metaStore = Maps.newConcurrentMap();
+
+
+    public JobService() {
+        LoadMetaDataFromDB();
+    }
+
+    public JobEntry get(long jobId) {
+        return metaStore.get(jobId);
+    }
+
+    public Map<Long, JobEntry> getMetaStore(){
+        return metaStore;
+    }
+
+
+//    public void addJob(Job job, List<ScheduleExpression> scheduleExpressions, Map<Long, JobDependencyEntry> dependencies) {
+//        metaStore.put(job.getJobId(), new JobEntry(job, scheduleExpressions, dependencies));
+//    }
+//
+//    public void remove(long jobId) {
+//        metaStore.remove(jobId);
+//    }
+
+
 
     /**
      * 获取活跃的job（状态为enable，并且不是过期的）
@@ -94,4 +134,119 @@ public class JobService {
         record.setUpdateTime(currentTime);
         jobMapper.updateByPrimaryKey(record);
     }
+
+
+    /**
+     * 读取metaData
+     */
+    private void LoadMetaDataFromDB(){
+
+        JobExample jobExample = new JobExample();
+        jobExample.createCriteria().andJobFlagEqualTo(JobFlag.ENABLE.getValue());
+        List<Job> jobs = jobMapper.selectByExample(jobExample);
+
+        List<JobScheduleExpression> scheduleExpressions = jobScheduleExpressionMapper.selectByExample(new JobScheduleExpressionExample());
+        Multimap<Long, ScheduleExpression> scheduleExpressionMap = ArrayListMultimap.create();
+        for (JobScheduleExpression jobScheduleExpression : scheduleExpressions) {
+            ScheduleExpression scheduleExpression = null;
+            int expressionType = jobScheduleExpression.getExpressionType();
+            long jobId = jobScheduleExpression.getJobId();
+
+            if (expressionType == ScheduleExpressionType.CRON.getValue()) {
+                scheduleExpression = new CronExpression(jobScheduleExpression.getExpression());
+            } else if (expressionType == ScheduleExpressionType.FIXED_RATE.getValue()) {
+                scheduleExpression = new FixedRateExpression(jobScheduleExpression.getExpression());
+            } else if (expressionType == ScheduleExpressionType.FIXED_DELAY.getValue()) {
+                scheduleExpression = new FixedDelayExpression(jobScheduleExpression.getExpression());
+            } else if (expressionType == ScheduleExpressionType.ISO8601.getValue()) {
+                scheduleExpression = new ISO8601Expression(jobScheduleExpression.getExpression());
+            }else{
+                LOGGER.warn("ExpressionType is undefined. id={};type={}",jobId,expressionType);
+                continue;
+            }
+
+            if (!scheduleExpression.isValid()) {
+                LOGGER.warn("expression value is invalid. id={};value={}",jobId,scheduleExpression.toString());
+                continue;
+            }
+
+            scheduleExpressionMap.put(jobId, scheduleExpression);
+        }
+
+        List<JobDepend> jobDepends = jobDependMapper.selectByExample(new JobDependExample());
+        Multimap<Long, JobDepend> jobDependMap = ArrayListMultimap.create();
+        for (JobDepend jobDepend : jobDepends) {
+            jobDependMap.put(jobDepend.getJobId(), jobDepend);
+        }
+
+        for (Job job : jobs) {
+            long jobId = job.getJobId();
+            List<ScheduleExpression> jobScheduleExpressions = new ArrayList<>(scheduleExpressionMap.get(jobId));
+            Map<Long, JobDependencyEntry> dependencies = null;
+            Collection<JobDepend> jobDependsCollection = jobDependMap.get(jobId);
+            if (jobDependsCollection != null && jobDependsCollection.size() > 0) {
+                dependencies = Maps.newHashMap();
+                for (JobDepend jobDepend : jobDependsCollection) {
+                    String offsetStrategy = jobDepend.getOffsetStrategy();
+                    if (offsetStrategy == null || offsetStrategy.isEmpty()) {
+                        offsetStrategy = "cd";
+                    }
+
+                    String commonStrategyStr = null;
+                    Integer commonStrategy = jobDepend.getCommonStrategy();
+                    if (commonStrategy == null) {
+                        commonStrategyStr = "*";
+                    } else {
+                        switch (commonStrategy) {
+                            case 1:
+                                commonStrategyStr = "L(1)";
+                                break;
+                            case 2:
+                                commonStrategyStr = "+";
+                                break;
+                            default:
+                                commonStrategyStr = "*";
+                                break;
+                        }
+                    }
+
+                    // 检查依赖表达式是否有效
+                    DependencyExpression dependencyExpression = new TimeOffsetExpression(offsetStrategy);
+                    if (!dependencyExpression.isValid()) {
+                        LOGGER.warn("dependency expression is invalid. id={}; value={}",jobId,dependencyExpression.toString());
+                        continue;
+                    }
+
+                    // 检查依赖策略表达式是否有效
+                    DependencyStrategyExpression dependencyStrategyExpression = new DefaultDependencyStrategyExpression(commonStrategyStr);
+                    if (!dependencyStrategyExpression.isValid()) {
+                        LOGGER.warn("dependency strategy is invalid. id={}; value={}",jobId,dependencyStrategyExpression.toString());
+                        continue;
+                    }
+
+                    JobDependencyEntry jobDependencyEntry = new JobDependencyEntry(dependencyExpression, dependencyStrategyExpression);
+                    dependencies.put(jobDepend.getPreJobId(), jobDependencyEntry);
+                }
+            }
+
+            // 初始化 JobMetaStore
+            metaStore.put(job.getJobId(), new JobEntry(job,jobScheduleExpressions, dependencies));
+
+//            // 初始化 JobGraph
+//            List<Long> dependencyJobIdList = null;
+//            if (dependencies != null && dependencies.size() > 0) {
+//                dependencyJobIdList = Lists.newArrayList();
+//                for (Map.Entry<Long, JobDependencyEntry> entry : dependencies.entrySet()) {
+//                    dependencyJobIdList.add(entry.getKey());
+//                }
+//            }
+//
+//            jobGraph.addJob(jobId, jobScheduleExpressions, dependencyJobIdList);
+        }
+
+
+    }
+
+
+
 }
