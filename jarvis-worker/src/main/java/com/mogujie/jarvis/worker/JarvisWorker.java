c3612fae3a3ab72488f7b1bfb2f24a245381f280
guangming <guangming@guangmingdeMacBook-Pro-2.local>
2015-12-02 11:02:51 +0800

    fix null pointer bug

--- jarvis-worker/src/main/java/com/mogujie/jarvis/worker/JarvisWorker.java
+++ jarvis-worker/src/main/java/com/mogujie/jarvis/worker/JarvisWorker.java
@@ -14,6 +14,17 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+import akka.actor.DeadLetter;
+import akka.pattern.Patterns;
+import akka.routing.SmallestMailboxPool;
+import akka.util.Timeout;
+
 import com.mogujie.jarvis.core.JarvisConstants;
 import com.mogujie.jarvis.core.util.ConfigUtils;
 import com.mogujie.jarvis.protocol.RegistryWorkerProtos.ServerRegistryResponse;
@@ -22,71 +33,52 @@ import com.mogujie.jarvis.worker.actor.DeadLetterActor;
 import com.mogujie.jarvis.worker.actor.WorkerActor;
 import com.typesafe.config.Config;
 
-import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
-import akka.actor.ActorSystem;
-import akka.actor.DeadLetter;
-import akka.pattern.Patterns;
-import akka.routing.SmallestMailboxPool;
-import akka.util.Timeout;
-import scala.concurrent.Await;
-import scala.concurrent.Future;
-import scala.concurrent.duration.Duration;
-
 public class JarvisWorker {
 
-  private static final Timeout TIMEOUT = new Timeout(Duration.create(30, TimeUnit.SECONDS));
-  private static final Logger LOGGER = LogManager.getLogger();
+    private static final Timeout TIMEOUT = new Timeout(Duration.create(30, TimeUnit.SECONDS));
+    private static final Logger LOGGER = LogManager.getLogger();
 
-  public static void main(String[] args) {
-    LOGGER.info("Starting jarvis worker...");
+    public static void main(String[] args) {
+        Config akkaConfig = ConfigUtils.getAkkaConfig("akka-worker.conf");
+        ActorSystem system = ActorSystem.create(JarvisConstants.WORKER_AKKA_SYSTEM_NAME, akkaConfig);
 
-    Config akkaConfig = ConfigUtils.getAkkaConfig("akka-worker.conf");
-    ActorSystem system = ActorSystem.create(JarvisConstants.WORKER_AKKA_SYSTEM_NAME, akkaConfig);
+        Configuration workerConfig = ConfigUtils.getWorkerConfig();
+        String serverAkkaPath = workerConfig.getString("server.akka.path") + JarvisConstants.SERVER_AKKA_USER_PATH;
+        int workerGroupId = workerConfig.getInt("worker.group.id", 0);
+        String workerKey = workerConfig.getString("worker.key");
+        WorkerRegistryRequest request = WorkerRegistryRequest.newBuilder().setKey(workerKey).build();
 
-    Configuration workerConfig = ConfigUtils.getWorkerConfig();
-    String serverAkkaPath = workerConfig.getString("server.akka.path")
-        + JarvisConstants.SERVER_AKKA_USER_PATH;
-    int workerGroupId = workerConfig.getInt("worker.group.id", 0);
-    String workerKey = workerConfig.getString("worker.key");
-    WorkerRegistryRequest request = WorkerRegistryRequest.newBuilder().setKey(workerKey).build();
+        // 注册Worker
+        ActorSelection serverActor = system.actorSelection(serverAkkaPath);
+        Future<Object> future = Patterns.ask(serverActor, request, TIMEOUT);
+        try {
+            ServerRegistryResponse response = (ServerRegistryResponse) Await.result(future, TIMEOUT.duration());
+            if (!response.getSuccess()) {
+                LOGGER.error("Worker register failed with group.id={}, worker.key={}", workerGroupId, workerKey);
+                system.terminate();
+                return;
+            }
+        } catch (Exception e) {
+            LOGGER.error("Worker register failed", e);
+            system.terminate();
+            return;
+        }
 
-    // 注册Worker
-    ActorSelection serverActor = system.actorSelection(serverAkkaPath);
-    Future<Object> future = Patterns.ask(serverActor, request, TIMEOUT);
-    try {
-      ServerRegistryResponse response = (ServerRegistryResponse) Await.result(future,
-          TIMEOUT.duration());
-      if (!response.getSuccess()) {
-        LOGGER.error("Worker register failed with group.id={}, worker.key={}", workerGroupId,
-            workerKey);
-        system.terminate();
-        return;
-      }
-    } catch (Exception e) {
-      LOGGER.error("Worker register failed", e);
-      system.terminate();
-      return;
+        ActorRef deadLetterActor = system.actorOf(new SmallestMailboxPool(10).props(DeadLetterActor.props()));
+        system.eventStream().subscribe(deadLetterActor, DeadLetter.class);
+
+        int actorNum = workerConfig.getInt("worker.actors.num", 100);
+        ActorRef workerActor = system.actorOf(new SmallestMailboxPool(actorNum).props(WorkerActor.props()), JarvisConstants.WORKER_AKKA_SYSTEM_NAME);
+
+        ActorSelection heartBeatActor = system.actorSelection(serverAkkaPath);
+
+        int heartBeatInterval = workerConfig.getInt("worker.heart.beat.interval.seconds", 5);
+
+        // 心跳汇报
+        system.scheduler().schedule(Duration.Zero(), Duration.create(heartBeatInterval, TimeUnit.SECONDS),
+                new HeartBeatThread(heartBeatActor, workerActor), system.dispatcher());
+
+        LOGGER.info("Starting jarvis worker...");
     }
-
-    ActorRef deadLetterActor = system
-        .actorOf(new SmallestMailboxPool(10).props(DeadLetterActor.props()));
-    system.eventStream().subscribe(deadLetterActor, DeadLetter.class);
-
-    int actorNum = workerConfig.getInt("worker.actors.num", 100);
-    ActorRef workerActor = system.actorOf(
-        new SmallestMailboxPool(actorNum).props(WorkerActor.props()),
-        JarvisConstants.WORKER_AKKA_SYSTEM_NAME);
-
-    ActorSelection heartBeatActor = system.actorSelection(serverAkkaPath);
-
-    int heartBeatInterval = workerConfig.getInt("worker.heart.beat.interval.seconds", 5);
-
-    // 心跳汇报
-    system.scheduler().schedule(Duration.Zero(),
-        Duration.create(heartBeatInterval, TimeUnit.SECONDS),
-        new HeartBeatThread(heartBeatActor, workerActor), system.dispatcher());
-
-  }
 
 }
