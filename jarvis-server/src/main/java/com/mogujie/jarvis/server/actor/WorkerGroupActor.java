e06f83f604d7aa8ab995e3f021ca471021adc5b7
muming <muming@mogujie.com>
2015-12-15 13:23:42 +0800

    workergroup

--- jarvis-server/src/main/java/com/mogujie/jarvis/server/actor/WorkerGroupActor.java
+++ jarvis-server/src/main/java/com/mogujie/jarvis/server/actor/WorkerGroupActor.java
@@ -8,18 +8,18 @@
 package com.mogujie.jarvis.server.actor;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Named;
 
+import com.mogujie.jarvis.server.service.WorkerGroupService;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
 import com.mogujie.jarvis.core.domain.ActorEntry;
 import com.mogujie.jarvis.core.domain.MessageType;
-import com.mogujie.jarvis.dao.generate.WorkerGroupMapper;
 import com.mogujie.jarvis.dto.generate.WorkerGroup;
 import com.mogujie.jarvis.protocol.WorkerGroupProtos.RestServerCreateWorkerGroupRequest;
 import com.mogujie.jarvis.protocol.WorkerGroupProtos.RestServerModifyWorkerGroupRequest;
@@ -33,49 +33,7 @@ import akka.actor.UntypedActor;
 public class WorkerGroupActor extends UntypedActor {
 
     @Autowired
-    private WorkerGroupMapper workerGroupMapper;
-
-    @Override
-    public void onReceive(Object obj) throws Exception {
-        if (obj instanceof RestServerCreateWorkerGroupRequest) {
-            RestServerCreateWorkerGroupRequest request = (RestServerCreateWorkerGroupRequest) obj;
-            WorkerGroup workerGroup = new WorkerGroup();
-            String key = UUID.randomUUID().toString().replace("-", "");
-            workerGroup.setAuthKey(key);
-            workerGroup.setName(request.getWorkerGroupName());
-            Date date = new Date();
-            workerGroup.setCreateTime(date);
-            workerGroup.setUpdateTime(date);
-            workerGroup.setUpdateUser(request.getUser());
-            workerGroupMapper.insertSelective(workerGroup);
-
-            ServerCreateWorkerGroupResponse response = ServerCreateWorkerGroupResponse.newBuilder().setSuccess(true).setWorkerGroupKey(key).build();
-            getSender().tell(response, getSelf());
-        } else if (obj instanceof RestServerModifyWorkerGroupRequest) {
-            RestServerModifyWorkerGroupRequest request = (RestServerModifyWorkerGroupRequest) obj;
-            WorkerGroup workerGroup = new WorkerGroup();
-            workerGroup.setId(request.getWorkerGroupId());
-            if (request.hasWorkerGroupName()) {
-                workerGroup.setName(request.getWorkerGroupName());
-            }
-
-            if (request.hasUser()) {
-                workerGroup.setUpdateUser(request.getUser());
-            }
-
-            if (request.hasStatus()) {
-                workerGroup.setStatus(request.getStatus());
-            }
-
-            workerGroup.setUpdateTime(new Date());
-            workerGroupMapper.updateByPrimaryKey(workerGroup);
-
-            ServerModifyWorkerGroupResponse response = ServerModifyWorkerGroupResponse.newBuilder().setSuccess(true).build();
-            getSender().tell(response, getSelf());
-        } else {
-            unhandled(obj);
-        }
-    }
+    private WorkerGroupService workerGroupService;
 
     public static List<ActorEntry> handledMessages() {
         List<ActorEntry> list = new ArrayList<>();
@@ -83,5 +41,64 @@ public class WorkerGroupActor extends UntypedActor {
         list.add(new ActorEntry(RestServerModifyWorkerGroupRequest.class, ServerModifyWorkerGroupResponse.class, MessageType.SYSTEM));
         return list;
     }
+
+    @Override
+    public void onReceive(Object obj) throws Exception {
+        if (obj instanceof RestServerCreateWorkerGroupRequest) {
+            createWorkerGroup((RestServerCreateWorkerGroupRequest) obj);
+        } else if (obj instanceof RestServerModifyWorkerGroupRequest) {
+            modifyWorkerGroup((RestServerModifyWorkerGroupRequest)obj);
+        } else {
+            unhandled(obj);
+        }
+    }
+
+    public void createWorkerGroup(RestServerCreateWorkerGroupRequest request) {
+        ServerCreateWorkerGroupResponse response;
+        try {
+            WorkerGroup workerGroup = new WorkerGroup();
+            String key = UUID.randomUUID().toString().replace("-", "");
+            workerGroup.setAuthKey(key);
+            workerGroup.setName(request.getWorkerGroupName());
+            DateTime now = DateTime.now();
+            workerGroup.setCreateTime(now.toDate());
+            workerGroup.setUpdateTime(now.toDate());
+            workerGroup.setUpdateUser(request.getUser());
+            workerGroupService.insert(workerGroup);
+
+            response = ServerCreateWorkerGroupResponse.newBuilder().setSuccess(true).setWorkerGroupKey(key).build();
+            getSender().tell(response, getSelf());
+        } catch (Exception ex) {
+            response = ServerCreateWorkerGroupResponse.newBuilder().setSuccess(false).setMessage(ex.getMessage()).build();
+            getSender().tell(response, getSelf());
+        }
+    }
+
+
+    public void modifyWorkerGroup(RestServerModifyWorkerGroupRequest request) {
+        ServerModifyWorkerGroupResponse response;
+        try {
+            WorkerGroup workerGroup = new WorkerGroup();
+            workerGroup.setId(request.getWorkerGroupId());
+            if (request.hasWorkerGroupName()) {
+                workerGroup.setName(request.getWorkerGroupName());
+            }
+            if (request.hasUser()) {
+                workerGroup.setUpdateUser(request.getUser());
+            }
+            if (request.hasStatus()) {
+                workerGroup.setStatus(request.getStatus());
+            }
+            workerGroup.setUpdateTime(DateTime.now().toDate());
+            workerGroup.setUpdateUser(request.getUser());
+            workerGroupService.update(workerGroup);
+            response = ServerModifyWorkerGroupResponse.newBuilder().setSuccess(true).build();
+            getSender().tell(response, getSelf());
+        } catch (Exception ex) {
+            response = ServerModifyWorkerGroupResponse.newBuilder().setSuccess(false).setMessage(ex.getMessage()).build();
+            getSender().tell(response, getSelf());
+        }
+    }
+
 
 }
