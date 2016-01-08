f4a79ce9c04a54cea51d211cda6039d0c7867920
hejian <hejian@mogujie.com>
2016-01-08 15:44:32 +0800

    no message

--- jarvis-core/src/main/java/com/mogujie/jarvis/core/domain/WorkerGroupStatus.java
+++ jarvis-core/src/main/java/com/mogujie/jarvis/core/domain/WorkerGroupStatus.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.mogujie.jarvis.core.domain;
 
 /**
@@ -13,16 +14,42 @@ public enum WorkerGroupStatus {
     WorkerGroupStatus(int value,String description){
         this.value=value;
         this.description=description;
+=======
+/*
+ * 蘑菇街 Inc.
+ * Copyright (c) 2010-2015 All Rights Reserved.
+ *
+ * Author: guangming
+ * Create Date: 2015年7月9日 下午16:55:35
+ */
+package com.mogujie.jarvis.core.domain;
+
+/**
+ * @author muming
+ */
+public enum WorkerGroupStatus {
+
+    ENABLE(1),      //有效
+    DISABLED(2);    //无效
+
+    private int value;
+
+    WorkerGroupStatus(int value) {
+        this.value = value;
+>>>>>>> master
     }
 
     public int getValue() {
         return value;
     }
 
+<<<<<<< HEAD
     public String getDescription() {
         return description;
     }
 
+=======
+>>>>>>> master
     public static Boolean isValid(int value) {
         WorkerGroupStatus[] values = WorkerGroupStatus.values();
         for (WorkerGroupStatus s : values) {
@@ -32,4 +59,18 @@ public enum WorkerGroupStatus {
         }
         return false;
     }
+<<<<<<< HEAD
+=======
+
+    public static WorkerGroupStatus parseValue(int value) throws IllegalArgumentException{
+        WorkerGroupStatus[] statusList = WorkerGroupStatus.values();
+        for (WorkerGroupStatus s : statusList) {
+            if (s.getValue() == value) {
+                return s;
+            }
+        }
+        throw new IllegalArgumentException("WorkerGroupStatus value is invalid. value:" + value);
+    }
+
+>>>>>>> master
 }
