a7b0d4f4c38804af43935e583c0184b11514f408
wuya <wuya@mogujie.com>
2016-01-12 20:35:03 +0800

    add ShellStatusLookup and YarnStatusLookup implementing

--- jarvis-worker/src/main/java/com/mogujie/jarvis/worker/status/lookup/ShellStatusLookup.java
+++ jarvis-worker/src/main/java/com/mogujie/jarvis/worker/status/lookup/ShellStatusLookup.java
@@ -8,28 +8,53 @@
 
 package com.mogujie.jarvis.worker.status.lookup;
 
-import org.apache.commons.configuration.Configuration;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.common.io.Files;
 import com.mogujie.jarvis.core.domain.TaskDetail;
+import com.mogujie.jarvis.core.domain.TaskStatus;
 import com.mogujie.jarvis.worker.status.TaskStatusLookup;
 
 public class ShellStatusLookup implements TaskStatusLookup {
 
+    private String dir = null;
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Override
     public void init(Configuration conf) {
-
+        dir = conf.getString("shell.status.data.dir");
     }
 
     @Override
     public int lookup(TaskDetail taskDetail) {
-        // TODO Auto-generated method stub
-        return 0;
+        String fullId = taskDetail.getFullId();
+        try {
+            File file = new File(dir + "/" + fullId + ".status");
+            if (file.exists()) {
+                String line = Files.readFirstLine(file, StandardCharsets.UTF_8);
+                switch (line.trim()) {
+                    case "0":
+                        return TaskStatus.SUCCESS.getValue();
+                    default:
+                        return TaskStatus.FAILED.getValue();
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("", e);
+        }
+
+        return TaskStatus.UNKNOWN.getValue();
     }
 
     @Override
     public void close() {
-        // TODO Auto-generated method stub
-
+        // ignore
     }
 
 }
--- jarvis-worker/src/main/java/com/mogujie/jarvis/worker/status/lookup/YarnStatusLookup.java
+++ jarvis-worker/src/main/java/com/mogujie/jarvis/worker/status/lookup/YarnStatusLookup.java
@@ -8,29 +8,100 @@
 
 package com.mogujie.jarvis.worker.status.lookup;
 
-import org.apache.commons.configuration.Configuration;
+import java.util.List;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.common.collect.Lists;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mogujie.jarvis.core.domain.TaskDetail;
+import com.mogujie.jarvis.core.domain.TaskStatus;
 import com.mogujie.jarvis.worker.status.TaskStatusLookup;
 
-public class YarnStatusLookup implements TaskStatusLookup {
+public abstract class YarnStatusLookup implements TaskStatusLookup {
+
+    private int activeUriIndex = 0;
+    private List<Object> yarnRestApiUris = null;
+    private static final String YARN_STATUS_LOOPUP_REST_URIS = "yarn.status.loopup.rest.uris";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    abstract public boolean accept(TaskDetail taskDetail, String appName);
 
     @Override
     public void init(Configuration conf) {
-        // TODO Auto-generated method stub
-
+        yarnRestApiUris = conf.getList(YARN_STATUS_LOOPUP_REST_URIS);
+        if (yarnRestApiUris == null || yarnRestApiUris.size() < 1) {
+            LOGGER.error("The value of " + YARN_STATUS_LOOPUP_REST_URIS + " is invalid");
+        }
     }
 
     @Override
     public int lookup(TaskDetail taskDetail) {
-        // TODO Auto-generated method stub
-        return 0;
+        for (int i = 0, len = yarnRestApiUris.size(); i < len; i++) {
+            List<Integer> statusList = Lists.newArrayList();
+            try {
+                HttpResponse<JsonNode> response = Unirest.get(yarnRestApiUris.get(activeUriIndex).toString()).asJson();
+                JSONArray appsJson = response.getBody().getObject().getJSONObject("apps").getJSONArray("app");
+                for (int j = 0, jlen = appsJson.length(); j < jlen; j++) {
+                    JSONObject appJson = appsJson.getJSONObject(j);
+                    String appName = appJson.getString("name");
+                    if (accept(taskDetail, appName)) {
+                        String finalStatus = appJson.getString("finalStatus");
+                        switch (finalStatus) {
+                            case "SUCCEEDED":
+                                statusList.add(TaskStatus.SUCCESS.getValue());
+                                break;
+                            case "FAILED":
+                                statusList.add(TaskStatus.FAILED.getValue());
+                                break;
+                            case "KILLED":
+                                statusList.add(TaskStatus.KILLED.getValue());
+                                break;
+                            case "RUNNING":
+                                statusList.add(TaskStatus.RUNNING.getValue());
+                                break;
+                            default:
+                                statusList.add(TaskStatus.UNKNOWN.getValue());
+                                break;
+                        }
+                    }
+                }
+
+                boolean allSuccess = true;
+                for (int status : statusList) {
+                    if (status != TaskStatus.SUCCESS.getValue()) {
+                        allSuccess = false;
+                        if (status != TaskStatus.UNKNOWN.getValue()) {
+                            return status;
+                        }
+                        break;
+                    }
+                }
+
+                if (allSuccess) {
+                    return TaskStatus.SUCCESS.getValue();
+                } else {
+                    return TaskStatus.UNKNOWN.getValue();
+                }
+            } catch (UnirestException | JSONException e) {
+                activeUriIndex = ++activeUriIndex % len;
+            }
+        }
+
+        return TaskStatus.UNKNOWN.getValue();
     }
 
     @Override
     public void close() {
-        // TODO Auto-generated method stub
-
+        // ignore
     }
 
 }
