9fc64886506084e06f27d92504a488b4b6e3b6c3
guangming <guangming@mogujie.com>
2016-01-19 16:12:03 +0800

    add log

--- jarvis-server/src/main/java/com/mogujie/jarvis/server/actor/HeartBeatActor.java
+++ jarvis-server/src/main/java/com/mogujie/jarvis/server/actor/HeartBeatActor.java
@@ -14,6 +14,10 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import akka.actor.Address;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+
 import com.mogujie.jarvis.core.domain.MessageType;
 import com.mogujie.jarvis.core.domain.WorkerInfo;
 import com.mogujie.jarvis.protocol.HeartBeatProtos.HeartBeatRequest;
@@ -22,10 +26,6 @@ import com.mogujie.jarvis.server.WorkerRegistry;
 import com.mogujie.jarvis.server.domain.ActorEntry;
 import com.mogujie.jarvis.server.guice.Injectors;
 import com.mogujie.jarvis.server.service.HeartBeatService;
-
-import akka.actor.Address;
-import akka.actor.Props;
-import akka.actor.UntypedActor;
 
 public class HeartBeatActor extends UntypedActor {
 
@@ -44,19 +44,20 @@ public class HeartBeatActor extends UntypedActor {
             Address address = getSender().path().address();
             String ip = address.host().get();
             int port = Integer.parseInt(address.port().get().toString());
-            int jobNum = request.getJobNum();
+            int taskNum = request.getJobNum();
+            LOGGER.info("receive heartbeat from worker[ip={},port={},taskNum={}]", ip, port, taskNum);
             WorkerInfo workerInfo = new WorkerInfo(ip, port);
             int groupId = Injectors.getInjector().getInstance(WorkerRegistry.class).getWorkerGroupId(workerInfo);
             HeartBeatResponse response = null;
             if (groupId < 0) {
-                LOGGER.warn("groupId is not valid: heartbeat[ip={}, port={}, groupId={}, jobNum={}]", ip, port, groupId, jobNum);
+                LOGGER.warn("groupId is not valid: heartbeat[ip={}, port={}, groupId={}, jobNum={}]", ip, port, groupId, taskNum);
                 //todo debug 测试临时屏蔽worker注册
-                heartBeatService.put(groupId, workerInfo, jobNum);
+                heartBeatService.put(groupId, workerInfo, taskNum);
                 //todo end.......
                 response = HeartBeatResponse.newBuilder().setSuccess(false).setMessage("groupId is not valid: " + groupId).build();
             } else {
-                LOGGER.debug("heartbeat[ip={}, port={}, groupId={}, jobNum={}]", ip, port, groupId, jobNum);
-                heartBeatService.put(groupId, workerInfo, jobNum);
+                LOGGER.debug("heartbeat[ip={}, port={}, groupId={}, jobNum={}]", ip, port, groupId, taskNum);
+                heartBeatService.put(groupId, workerInfo, taskNum);
                 response = HeartBeatResponse.newBuilder().setSuccess(true).build();
             }
             getSender().tell(response, getSelf());
