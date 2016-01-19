c6dc7b7a2cac06ca5b6dd0e2c4ab43aead123cd7
wuya <wuya@mogujie.com>
2016-01-19 16:47:37 +0800

    fix file separator

---
+++ jarvis-worker/src/test/java/com/mogujie/jarvis/worker/actor/Test.java
@@ -0,0 +1,19 @@
+/*
+ * 蘑菇街 Inc.
+ * Copyright (c) 2010-2016 All Rights Reserved.
+ *
+ * Author: wuya
+ * Create Date: 2016年1月19日 下午4:38:45
+ */
+
+package com.mogujie.jarvis.worker.actor;
+
+import java.io.File;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.io.tmpdir"));
+        System.out.println(File.separator);
+    }
+}
