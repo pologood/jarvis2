dcf06bed29219f594db36b97fa346fb1752105e5
qinghuo <qinghuo@mogujie.com>
2016-01-21 03:11:32 +0800

    add job test case

--- jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestJobActor.java
+++ jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestJobActor.java
@@ -2,15 +2,24 @@ package com.mogujie.jarvis.server.actor;
 
 import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
-import akka.testkit.JavaTestKit;
+import com.mogujie.jarvis.core.domain.CommonStrategy;
+import com.mogujie.jarvis.core.domain.JobPriority;
+import com.mogujie.jarvis.core.domain.JobStatus;
+import com.mogujie.jarvis.core.domain.OperationMode;
 import com.mogujie.jarvis.core.util.ConfigUtils;
+import com.mogujie.jarvis.core.util.IPUtils;
 import com.mogujie.jarvis.dto.generate.JobDepend;
 import com.mogujie.jarvis.dto.generate.JobDependKey;
-import com.mogujie.jarvis.protocol.AppAuthProtos;
+import com.mogujie.jarvis.protocol.AppAuthProtos.AppAuth;
+import com.mogujie.jarvis.protocol.JobDependencyEntryProtos.DependencyEntry;
+import com.mogujie.jarvis.protocol.JobProtos.RestSubmitJobRequest;
+import com.mogujie.jarvis.protocol.JobProtos.ServerSubmitJobResponse;
 import com.mogujie.jarvis.server.actor.base.DBTestBased;
+import com.mogujie.jarvis.server.domain.JobEntry;
 import com.mogujie.jarvis.server.guice4test.Injectors4Test;
 import com.mogujie.jarvis.server.service.JobService;
 import com.mogujie.jarvis.server.util.AppTokenUtils;
+import com.mogujie.jarvis.server.util.FutureUtils;
 import com.typesafe.config.Config;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.ReplacementDataSet;
@@ -21,9 +30,12 @@ import org.junit.Test;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Location www.mogujie.com
@@ -33,9 +45,8 @@ import static org.junit.Assert.assertEquals;
 public class TestJobActor extends DBTestBased {
     Config config = ConfigUtils.getAkkaConfig("akka-test.conf");
     String authKey = AppTokenUtils.generateToken(new Date().getTime(), "00416f0458d2482994d70be4b32ab756");
-    AppAuthProtos.AppAuth appAuth = AppAuthProtos.AppAuth.newBuilder().setName("a").setToken(authKey).build();
-    ActorSystem system = ActorSystem.create("worker", config);
-    JobService jobService = Injectors4Test.getInjector().getInstance(JobService.class);
+    ActorSystem system;
+    JobService jobService ;
     Connection conn = null;
     Boolean result = Boolean.FALSE;
     IDatabaseConnection iconn = null;
@@ -71,8 +82,6 @@ public class TestJobActor extends DBTestBased {
     }
 
 
-
-
     @Override
     protected void prepareData(IDatabaseConnection iconn, String tableName) throws Exception {
         //Remove the data from table app
@@ -97,6 +106,69 @@ public class TestJobActor extends DBTestBased {
             e.printStackTrace();
         }
     }
+
+    /**
+     * a
+     * |
+     * b
+     */
+
+    @Test
+    public void testSubmitJob() {
+        AppAuth appAuth = AppAuth.newBuilder().setToken("e162b634a881453fb26149cbcb68b2a7").setName("jarvis-web").build();
+        Config akkaConfig = ConfigUtils.getAkkaConfig("akka-test.conf");
+        system = ActorSystem.create("rest", akkaConfig);
+        String actorPath = "akka.tcp://server@" + IPUtils.getIPV4Address() + ":10000/user/server";
+        ActorSelection serverActor = system.actorSelection(actorPath);
+        DependencyEntry dependencyEntry = DependencyEntry.newBuilder()
+                .setCommonDependStrategy(CommonStrategy.ALL.getValue())
+                .setJobId(2L).setOffsetDependStrategy("d(3)")
+                .setOperator(OperationMode.ADD.getValue())
+                .build();
+        //添加依赖
+        List<DependencyEntry> dependencyEntryList = new ArrayList<DependencyEntry>();
+        dependencyEntryList.add(dependencyEntry);
+        RestSubmitJobRequest request = RestSubmitJobRequest.newBuilder()
+                .addAllDependencyEntry(dependencyEntryList)
+                .setJobName("qh_test")
+                .setAppName("jarvis-web")
+                .setAppAuth(appAuth)
+                .setContent("use testing")
+                .setPriority(JobPriority.HIGH.getValue())
+                .setParameters("{\"para1\":\"1\",\"para2\":\"2\"}")
+                .setStatus(JobStatus.ENABLE.getValue())
+                .setUser("qinghuo")
+                .setExpiredTime(86400).setFailedAttempts(3)
+                .setFailedInterval(3)
+                .setBizGroupId(11).setJobType("hive script")
+                .setWorkerGroupId(1)
+                .build();
+        ServerSubmitJobResponse response = null;
+        try {
+            response = (ServerSubmitJobResponse) FutureUtils.awaitResult(serverActor, request, 30);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertTrue(response.getSuccess());
+        JobDependKey key = new JobDependKey();
+        key.setJobId(response.getJobId());
+        jobService=Injectors4Test.getInjector().getInstance(JobService.class);
+        JobDepend jobDepend = jobService.getJobDepend(key);
+        JobEntry jobEntry=jobService.get(response.getJobId());
+        try {
+            assertEquals(jobEntry.getJob().getJobName(), "qh_test");
+            assertTrue(jobService.isActive(response.getJobId()));
+            assertEquals((long) jobDepend.getPreJobId(), 2L);
+        } catch (Exception ex) {
+        } finally {
+            jobService.deleteJob(response.getJobId());
+            jobService.deleteJobDepend(response.getJobId(), 2L);
+        }
+
+
+    }
+
+
 
     @After
     public void tearDown() {
--- jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestWorkerActor.java
+++ jarvis-server/src/test/java/com/mogujie/jarvis/server/actor/TestWorkerActor.java
@@ -8,42 +8,32 @@ import com.mogujie.jarvis.core.util.ConfigUtils;
 import com.mogujie.jarvis.core.util.IPUtils;
 import com.mogujie.jarvis.dto.generate.Worker;
 import com.mogujie.jarvis.protocol.ModifyWorkerStatusProtos;
-import com.mogujie.jarvis.protocol.RegistryWorkerProtos;
+import com.mogujie.jarvis.protocol.WorkerProtos.ServerRegistryResponse;
+import com.mogujie.jarvis.protocol.WorkerProtos.WorkerRegistryRequest;
 import com.mogujie.jarvis.server.JarvisServer;
-import com.mogujie.jarvis.server.actor.base.DBTestBased;
 import com.mogujie.jarvis.server.actor.util.TestUtil;
 import com.mogujie.jarvis.server.guice4test.Injectors4Test;
 import com.mogujie.jarvis.server.service.WorkerService;
+import com.mogujie.jarvis.server.util.FutureUtils;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigValueFactory;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.ReplacementDataSet;
-import org.dbunit.operation.DatabaseOperation;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mybatis.guice.transactional.Transactional;
 
 import java.io.IOException;
-import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 /**
  * Location www.mogujie.com
  * Created by qinghuo on 16/1/14.
  * used by jarvis-parent
  */
-public class TestWorkerActor  {
-    String authKey = "d03fa97612734db7bdee3bbb2cdbf993";
+public class TestWorkerActor {
+    String authKey = "ec80df2716a547b89d99a3d135dea1d3";
     Thread threadServer = null;
-    Connection conn = null;
-    Boolean result = Boolean.FALSE;
-    IDatabaseConnection iconn = null;
-    int registPort = 10004;
+    int registPort = 10001;
     Injector injector = Injectors4Test.getInjector();
     WorkerService workerService = injector.getInstance(WorkerService.class);
     ActorSystem system;
@@ -53,7 +43,7 @@ public class TestWorkerActor  {
         try {
 //检测server端口是否被占用
             if (TestUtil.isPortHasBeenUse("localhost", 10000) && TestUtil.isPortHasBeenUse(InetAddress.getLocalHost().getHostAddress(), 10000)
-                    &&TestUtil.isPortHasBeenUse(IPUtils.getIPV4Address(),10000)) {
+                    && TestUtil.isPortHasBeenUse(IPUtils.getIPV4Address(), 10000)) {
                 ServerProxy serverProxy = new ServerProxy();
                 threadServer = new Thread(serverProxy);
                 threadServer.start();
@@ -69,10 +59,12 @@ public class TestWorkerActor  {
     public void testModifyWorkerStatus() {
         Config akkaConfig = ConfigUtils.getAkkaConfig("akka-test.conf");
         String portPath = "akka.remote.netty.tcp.port";
+        String hostPath = "akka.remote.netty.tcp.hostname";
+        String hostname = IPUtils.getIPV4Address();
         akkaConfig = akkaConfig.withValue(portPath, ConfigValueFactory.fromAnyRef(registPort));
-        system = ActorSystem.create("worker", akkaConfig);
+        akkaConfig = akkaConfig.withValue(hostPath, ConfigValueFactory.fromAnyRef(hostname));
+         system = ActorSystem.create("worker", akkaConfig);
         ActorSelection serverActor = system.actorSelection("akka.tcp://server@192.168.21.82:10000/user/server");
-
 
 
         ModifyProxy proxy1 = new ModifyProxy(10006);
@@ -82,18 +74,8 @@ public class TestWorkerActor  {
 
     @After
     public void tearDown() {
-        system.shutdown();
+        //system.shutdown();
         if (threadServer != null) threadServer.interrupt();
-        //just remember to rollback database
-        if (conn != null) {
-            try {
-                conn.rollback();
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        }
 
     }
 
@@ -103,19 +85,27 @@ public class TestWorkerActor  {
         //测试绑定10003至10004端口
         Config akkaConfig = ConfigUtils.getAkkaConfig("akka-test.conf");
         String portPath = "akka.remote.netty.tcp.port";
+        String hostPath = "akka.remote.netty.tcp.hostname";
+        String hostname = IPUtils.getIPV4Address();
+        // akkaConfig = akkaConfig.withValue(hostPath, ConfigValueFactory.fromAnyRef(hostname));
         akkaConfig = akkaConfig.withValue(portPath, ConfigValueFactory.fromAnyRef(registPort));
-        system = ActorSystem.create("worker", akkaConfig);
-        ActorSelection serverActor = system.actorSelection("akka.tcp://server@"+IPUtils.getIPV4Address()+":10000/user/server");
+
+         system = ActorSystem.create("worker", akkaConfig);
+        String serverPath = "akka.tcp://server@"+IPUtils.getIPV4Address()+":10000/user/server";
+        ActorSelection serverActor = system.actorSelection(serverPath);
+        WorkerRegistryRequest workerRegistryRequest =
+                WorkerRegistryRequest.newBuilder().setKey(authKey).build();
 
         new JavaTestKit(system) {{
-            RegistryWorkerProtos.WorkerRegistryRequest workerRegistryRequest = RegistryWorkerProtos.WorkerRegistryRequest.newBuilder().setKey(authKey).build();
+
             int flag = 0;
             while (flag < 10) {
                 try {
                     serverActor.tell(workerRegistryRequest, getRef());
 
-                    RegistryWorkerProtos.ServerRegistryResponse response
-                            = (RegistryWorkerProtos.ServerRegistryResponse) receiveOne(duration("3 seconds"));
+                    ServerRegistryResponse response
+                            = (ServerRegistryResponse) receiveOne(duration("3 seconds"));
+
                     if (response.getSuccess()) {
                         Assert.assertEquals(response.getSuccess(), true);
                         break;
@@ -124,11 +114,13 @@ public class TestWorkerActor  {
 
                 } catch (NullPointerException ex) {
                     System.err.println("server not ready");
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
 
                 flag++;
                 try {
-                    Thread.currentThread().sleep(1000);
+                    Thread.currentThread().sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -136,9 +128,8 @@ public class TestWorkerActor  {
         }};
 
 
-        Assert.assertEquals(workerService.getWorkerId("127.0.0.1", registPort), 11);
+        Assert.assertEquals(workerService.getWorkerId(IPUtils.getIPV4Address(), registPort), 22);
     }
-
 
 
     class ModifyProxy implements Runnable {
