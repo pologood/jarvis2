82fea25119207195057c265f3464e6dc1138be8d
guangming <guangming@mogujie.com>
2016-01-21 19:49:38 +0800

    add build.sh and update some properties

--- bin/buildDist.sh
+++ bin/buildDist.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 
+version='jarvis-2.0-bin'
+
 bin=`dirname "${BASH_SOURCE-$0}"`
 bin=`cd "$bin"; pwd`
 
 JARVIS_PREFIX=`dirname "$bin"`
-JARVIS_TARGET_LIB_DIR="${JARVIS_PREFIX}/lib"
+JARVIS_DIST_DIR="${JARVIS_PREFIX}/dist"
+JARVIS_TARGET_DIR="${JARVIS_DIST_DIR}/${version}"
+JARVIS_TARGET_LIB_DIR="${JARVIS_TARGET_DIR}/lib"
 
 mvn clean install -Dmaven.test.skip=true -f ${JARVIS_PREFIX}/pom.xml
 
-echo "start copy lib..."
+echo "Packaging..."
 
 mkdir -p ${JARVIS_TARGET_LIB_DIR}/common
 mkdir -p ${JARVIS_TARGET_LIB_DIR}/rest
@@ -24,5 +28,11 @@ find ./jarvis-rest/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/rest 
 find ./jarvis-server/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/server \;
 find ./jarvis-worker/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/worker \;
 find ./jarvis-tasks/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/worker \;
+mkdir -p ${JARVIS_TARGET_DIR}/bin
+cp ${JARVIS_PREFIX}/bin/jarvis-* ${JARVIS_TARGET_DIR}/bin/
+cp -r ${JARVIS_PREFIX}/conf ${JARVIS_TARGET_DIR}/conf
+mkdir -p ${JARVIS_TARGET_DIR}/logs
+cd ${JARVIS_DIST_DIR} && tar zcf ${version}.tar.gz ${version} && rm -rf ${version}
 
-echo "build finished"
+
+echo "Package finished: ${JARVIS_DIST_DIR}/${version}.tar.gz"
