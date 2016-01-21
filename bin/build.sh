efbcb306044495bcad99221c2737c0266f867009
wuya <wuya@mogujie.com>
2016-01-21 10:23:52 +0800

    update

--- bin/build.sh
+++ bin/build.sh
@@ -9,10 +9,22 @@ bin=`cd "$bin"; pwd`
 JARVIS_PREFIX=`dirname "$bin"`
 JARVIS_DIST_DIR="${JARVIS_PREFIX}/dist"
 JARVIS_TARGET_DIR="${JARVIS_DIST_DIR}/${version}"
+JARVIS_TARGET_LIB_DIR="${JARVIS_TARGET_DIR}/lib"
 
 mvn clean install -DskipTests -f ${JARVIS_PREFIX}/pom.xml
-mkdir -p ${JARVIS_TARGET_DIR}/lib
-find ./ -name *.jar ! -path "./jarvis-web/*" -exec cp {} ${JARVIS_TARGET_DIR}/lib \;
+mkdir -p ${JARVIS_TARGET_LIB_DIR}/common
+mkdir -p ${JARVIS_TARGET_LIB_DIR}/rest
+mkdir -p ${JARVIS_TARGET_LIB_DIR}/server
+mkdir -p ${JARVIS_TARGET_LIB_DIR}/worker
+mkdir -p ${JARVIS_TARGET_LIB_DIR}/logstorage
+find ./jarvis-core/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/common \;
+find ./jarvis-dao/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/common \;
+find ./jarvis-protocol/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/common \;
+find ./jarvis-logstorage/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/logstorage \;
+find ./jarvis-rest/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/rest \;
+find ./jarvis-server/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/server \;
+find ./jarvis-worker/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/worker \;
+find ./jarvis-tasks/target -name *.jar -exec cp {} ${JARVIS_TARGET_LIB_DIR}/worker \;
 mkdir -p ${JARVIS_TARGET_DIR}/bin
 cp ${JARVIS_PREFIX}/bin/jarvis-* ${JARVIS_TARGET_DIR}/bin/
 cp -r ${JARVIS_PREFIX}/conf ${JARVIS_TARGET_DIR}/conf
