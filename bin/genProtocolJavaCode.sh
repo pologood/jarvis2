3717d02434ee8b1eeee5e81d2cca40d6bc108ef8
muming <muming@mumingdeMacBook-Pro.local>
2016-01-26 11:43:10 +0800

    shell scropt edit

--- bin/genProtocolJavaCode.sh
+++ bin/genProtocolJavaCode.sh
@@ -1,1 +1,7 @@
-protoc   --java_out=../jarvis-protocol/src/main/java  --proto_path=../jarvis-protocol/src/main/resources/protos ../jarvis-protocol/src/main/resources/protos/*.proto
+#!/usr/bin/env bash
+
+bin=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+JARVIS_HOME=`dirname "$bin"`
+JARVIS_PROTOCOL_MAIN="${JARVIS_HOME}/jarvis-protocol/src/main"
+
+protoc --java_out=${JARVIS_PROTOCOL_MAIN}/java --proto_path=${JARVIS_PROTOCOL_MAIN}/resources/protos ${JARVIS_PROTOCOL_MAIN}/resources/protos/*.proto
