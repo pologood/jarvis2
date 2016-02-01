e6abf69cf28718c3388ca843398f791af382f9a2
wuya <wuya@mogujie.com>
2016-02-01 16:13:56 +0800

    update TaskStateStoreFactory and remove useless configuration

--- jarvis-worker/src/main/java/com/mogujie/jarvis/worker/status/TaskStateStoreFactory.java
+++ jarvis-worker/src/main/java/com/mogujie/jarvis/worker/status/TaskStateStoreFactory.java
@@ -15,19 +15,23 @@ import com.mogujie.jarvis.core.util.ConfigUtils;
 import com.mogujie.jarvis.core.util.ReflectionUtils;
 import com.mogujie.jarvis.worker.WorkerConfigKeys;
 
-public class TaskStateStoreFactory {
+public final class TaskStateStoreFactory {
 
     private static Configuration config = ConfigUtils.getWorkerConfig();
     private static TaskStateStore taskStateStore = null;
 
     static {
         try {
-            String className = config.getString(WorkerConfigKeys.WORKER_TASK_STATE_STORE_CLASS, WorkerConfigKeys.DEFAULT_WORKER_TASK_STATE_STORE_CLASS);
+            String className = config.getString(WorkerConfigKeys.WORKER_TASK_STATE_STORE_CLASS,
+                    WorkerConfigKeys.DEFAULT_WORKER_TASK_STATE_STORE_CLASS);
             taskStateStore = ReflectionUtils.getInstanceByClassName(className);
             taskStateStore.init(config);
         } catch (Exception e) {
             Throwables.propagate(e);
         }
+    }
+
+    private TaskStateStoreFactory() {
     }
 
     public static TaskStateStore getInstance() {
