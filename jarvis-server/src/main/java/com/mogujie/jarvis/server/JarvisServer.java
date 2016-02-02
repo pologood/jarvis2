b99755158e80a8d6397df79e8158b110cb94fcf5
guangming <guangming@mogujie.com>
2016-02-02 17:39:36 +0800

    add plan

--- jarvis-server/src/main/java/com/mogujie/jarvis/server/JarvisServer.java
+++ jarvis-server/src/main/java/com/mogujie/jarvis/server/JarvisServer.java
@@ -8,9 +8,11 @@
 
 package com.mogujie.jarvis.server;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Timer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -59,6 +61,7 @@ import com.mogujie.jarvis.server.scheduler.task.TaskScheduler;
 import com.mogujie.jarvis.server.scheduler.time.TimePlan;
 import com.mogujie.jarvis.server.scheduler.time.TimeScheduler;
 import com.mogujie.jarvis.server.service.JobService;
+import com.mogujie.jarvis.server.service.PlanService;
 import com.mogujie.jarvis.server.service.TaskService;
 import com.mogujie.jarvis.server.util.PlanUtil;
 
@@ -100,7 +103,7 @@ public class JarvisServer {
 
     public static void init() throws Exception {
         initScheduler();
-        // initTimerTask();
+        initTimerTask();
     }
 
     private static void initScheduler() throws JobScheduleException, CycleFoundException {
@@ -220,5 +223,20 @@ public class JarvisServer {
         for (Task task : readyTasks) {
             controller.notify(new RetryTaskEvent(task.getJobId(), task.getTaskId()));
         }
+    }
+
+    private static void initTimerTask() {
+        final String startTime = "23:30:00";
+        final long time24h = 24 * 60 * 60 * 1000;
+        String currentDate = DateTime.now().toString("yyyy-MM-dd");
+        Date firstTime = (new DateTime(currentDate + "T" + startTime)).toDate();
+        Timer timer = new Timer();
+        // init PlanTimerTask
+        timer.scheduleAtFixedRate(new PlanTimerTask(), firstTime, time24h);
+        // 系统启动的时候自动更新执行计划
+        PlanService planService = Injectors.getInjector().getInstance(PlanService.class);
+        DateTime now = DateTime.now();
+        Range<DateTime> range = Range.closedOpen(now.withTimeAtStartOfDay(), now.plusDays(1).withTimeAtStartOfDay());
+        planService.updateJobIds(range);
     }
 }
