26209935f2b5f3d6f8bf48fff8338359eb68a1ca
muming <muming@mumingdeMacBook-Pro.local>
2016-02-02 15:31:40 +0800

    dao plan add

---
+++ jarvis-dao/src/main/java/com/mogujie/jarvis/dao/generate/PlanMapper.java
@@ -0,0 +1,30 @@
+package com.mogujie.jarvis.dao.generate;
+
+import com.mogujie.jarvis.dto.generate.Plan;
+import com.mogujie.jarvis.dto.generate.PlanExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PlanMapper {
+    int countByExample(PlanExample example);
+
+    int deleteByExample(PlanExample example);
+
+    int deleteByPrimaryKey(Long jobId);
+
+    int insert(Plan record);
+
+    int insertSelective(Plan record);
+
+    List<Plan> selectByExample(PlanExample example);
+
+    Plan selectByPrimaryKey(Long jobId);
+
+    int updateByExampleSelective(@Param("record") Plan record, @Param("example") PlanExample example);
+
+    int updateByExample(@Param("record") Plan record, @Param("example") PlanExample example);
+
+    int updateByPrimaryKeySelective(Plan record);
+
+    int updateByPrimaryKey(Plan record);
+}
---
+++ jarvis-dao/src/main/java/com/mogujie/jarvis/dto/generate/Plan.java
@@ -0,0 +1,35 @@
+package com.mogujie.jarvis.dto.generate;
+
+import java.util.Date;
+
+public class Plan {
+    private Long jobId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
