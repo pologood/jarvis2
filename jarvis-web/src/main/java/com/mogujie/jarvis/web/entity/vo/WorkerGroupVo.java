25dd549c9356a8399993097ea814873dc94fe2c3
hejian <hejian@mogujie.com>
2016-02-06 11:54:49 +0800

    no message

--- jarvis-web/src/main/java/com/mogujie/jarvis/web/entity/vo/WorkerGroupVo.java
+++ jarvis-web/src/main/java/com/mogujie/jarvis/web/entity/vo/WorkerGroupVo.java
@@ -6,31 +6,5 @@ import com.mogujie.jarvis.dto.generate.WorkerGroup;
  * Created by hejian on 15/9/28.
  */
 public class WorkerGroupVo extends WorkerGroup {
-    private String createTimeStr;
-    private String updateTimeStr;
-    private String statusStr;
 
-    public String getCreateTimeStr() {
-        return createTimeStr;
-    }
-
-    public void setCreateTimeStr(String createTimeStr) {
-        this.createTimeStr = createTimeStr;
-    }
-
-    public String getUpdateTimeStr() {
-        return updateTimeStr;
-    }
-
-    public void setUpdateTimeStr(String updateTimeStr) {
-        this.updateTimeStr = updateTimeStr;
-    }
-
-    public String getStatusStr() {
-        return statusStr;
-    }
-
-    public void setStatusStr(String statusStr) {
-        this.statusStr = statusStr;
-    }
 }
