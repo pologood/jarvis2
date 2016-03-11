3f5f6efcaec2488f2eeea13d98ac072080685afb
muming <muming@mumingdeMacBook-Pro.local>
2016-03-11 15:30:59 +0800

    show msg mover

--- jarvis-web/src/main/webapp/assets/js/jarvis.js
+++ jarvis-web/src/main/webapp/assets/js/jarvis.js
@@ -1,3 +1,13 @@
+function showMsg(type, title, text) {
+    new PNotify({
+        title: title,
+        text: text,
+        type: type,
+        icon: true,
+        styling: 'bootstrap3'
+    });
+}
+
 //时间选择器
 function createDatetimePickerById(tagId) {
     if (tagId == undefined || tagId == '') {
@@ -25,43 +35,19 @@ function requestRemoteRestApi(url, title, data) {
             if (data.code == 0) {
                 flag = true;
                 if (data.msg == null || data.msg == '') {
-                    new PNotify({
-                        title: title,
-                        text: '操作成功',
-                        type: 'success',
-                        icon: true,
-                        styling: 'bootstrap3'
-                    });
+                    showMsg('success', title, '操作成功');
                 }
                 else {
-                    new PNotify({
-                        title: title,
-                        text: data.msg,
-                        type: 'success',
-                        icon: true,
-                        styling: 'bootstrap3'
-                    });
+                    showMsg('success', title, data.msg);
                 }
             }
             else {
                 flag = false;
                 if (data.msg == null || data.msg == '') {
-                    new PNotify({
-                        title: title,
-                        text: '操作失败',
-                        type: 'warning',
-                        icon: true,
-                        styling: 'bootstrap3'
-                    });
+                    showMsg('warning', title, '操作失败');
                 }
                 else {
-                    new PNotify({
-                        title: title,
-                        text: data.msg,
-                        type: 'warning',
-                        icon: true,
-                        styling: 'bootstrap3'
-                    });
+                    showMsg('warning', title, data.msg);
                 }
             }
             result["data"] = data;
