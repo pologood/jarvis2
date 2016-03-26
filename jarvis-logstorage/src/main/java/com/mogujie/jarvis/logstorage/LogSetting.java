32f9ec2f9da73b6ed557ecb3b1684385af19601b
wuya <wuya@mogujie.com>
2016-03-26 14:46:19 +0800

    fix read log bug

--- jarvis-logstorage/src/main/java/com/mogujie/jarvis/logstorage/LogSetting.java
+++ jarvis-logstorage/src/main/java/com/mogujie/jarvis/logstorage/LogSetting.java
@@ -13,6 +13,6 @@ import com.mogujie.jarvis.core.util.ConfigUtils;
 public class LogSetting {
 
     public static final String LOG_LOCAL_PATH = ConfigUtils.getLogstorageConfig().getString("log.path.local", "/tmp/logs");
-    public static final int LOG_READ_MAX_SIZE = ConfigUtils.getLogstorageConfig().getInt("log.read.size.max", 10000);
+    public static final int LOG_READ_MAX_SIZE = ConfigUtils.getLogstorageConfig().getInt("log.read.size.max", 1024 * 1024);
 
 }
