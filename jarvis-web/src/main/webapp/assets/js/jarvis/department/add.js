b1c86b3c3ce176ef66ff59718968cf048fab4855
qingyuan <qingyuan@mogujie.com>
2016-03-29 20:03:04 +0800

    fix

--- jarvis-web/src/main/webapp/assets/js/jarvis/department/add.js
+++ jarvis-web/src/main/webapp/assets/js/jarvis/department/add.js
@@ -41,7 +41,9 @@ function saveBzGroup() {
     window.setTimeout(function () {
       showMsg('info', '产品线', '保存成功');
     }, 1000);
-    window.location.reload();
+    window.setTimeout(function () {
+      window.location.reload();
+    }, 1000)
     $("#departmentInfoDiv").removeClass("active");
     $("#bizGroupInfoDiv").removeClass("active");
     $("#ralationInfoDiv").addClass("active");
